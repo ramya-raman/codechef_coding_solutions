8c345bf8756a7aeb36741b452b21a5cc8c63a19b
Ramya K Raman <ramya.kariat@gmail.com>
2019-02-27 12:02:13 +0530

    Chef & Difficult contests solution(Code: CHFTIRED)
    This is a C++ solution for CodeChef contest November Challenge 2018 Division 2 problem Chef and Difficult contests
    Problem link: https://www.codechef.com/NOV18B/problems/CHFTIRED

---
+++ chftired_soln.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+ {
+	//code
+	ios_base::sync_with_stdio(false);
+	long T;
+	long A, B;
+	
+	cin >> T;
+	while(T--){
+	    cin >> A >> B;
+	    cout << "YES" << endl;
+	}
+	
+	return 0;
+}
